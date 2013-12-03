ecf062cacbbb5a4d74c53ec2e4a4ef4224a72cca
sherwin <sherwin@sherwins-Mac-mini.local>
2013-12-03 18:12:24 +0800

    add  RSSListEntity

---
+++ iRSS/Classes/Moudules/RSSList_C/Models/RSSListEntity.h
@@ -0,0 +1,13 @@
+//
+//  RSSListEntity.h
+//  iRSS
+//
+//  Created by sherwin on 13-12-3.
+//  Copyright (c) 2013年 sherwin. All rights reserved.
+//
+
+#import "Entity.h"
+
+@interface RSSListEntity : Entity
+
+@end
---
+++ iRSS/Classes/Moudules/RSSList_C/Models/RSSListEntity.m
@@ -0,0 +1,13 @@
+//
+//  RSSListEntity.m
+//  iRSS
+//
+//  Created by sherwin on 13-12-3.
+//  Copyright (c) 2013年 sherwin. All rights reserved.
+//
+
+#import "RSSListEntity.h"
+
+@implementation RSSListEntity
+
+@end
